745282cc7a4b508536834a870368795f50168c5d
mmgcarlos <cs@cs>
2023-04-18 16:46:28 +0100

    Using terraform-acuris-ecs-container-definition 2.2.0

--- main.tf
+++ main.tf
@@ -49,8 +49,8 @@ module "taskdef" {
 }
 
 module "service_container_definition" {
-  source  = "github.com/mmgcarlos/terraform-acuris-ecs-container-definition"
-  # version = "2.1.0"
+  source  = "mergermarket/ecs-container-definition/acuris"
+  version = "2.2.0"
 
   name                = "${var.release["component"]}${var.name_suffix}"
   image               = "${var.image_id != "" ? var.image_id : var.release["image_id"]}"
