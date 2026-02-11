5fa7ce8d26056bed69b0003080441bcb2cdfd530
Keir Badger <keir.badger@iongroup.com>
2026-02-11 09:25:59 +0000

    Use ordered_placement change for all types of service

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "ecs_update_monitor" {
 
 module "service" {
   source  = "mergermarket/load-balanced-ecs-service-no-target-group/acuris"
-  version = "2.6.2"
+  version = "2.7.1"
 
   name                                  = local.full_service_name
   cluster                               = var.ecs_cluster
